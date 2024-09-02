f57a926e8c752c82567b0a5814021cae76ffa93f
IceB3ar18 <165510868+IceB3ar18@users.noreply.github.com>
2024-09-02 18:32:26 +0200

    test

--- account/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Alle Session-Variablen löschen
-session_destroy(); // Sitzung zerstören
-
-header("Location: login.html"); // Weiterleitung zur Login-Seite
-exit();
-?>
